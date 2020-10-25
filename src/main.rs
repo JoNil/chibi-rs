7a8b4c6261fb9ae09c0fd36a52b02d4003312faa
Jonathan Nilsson <jonathan@voysys.se>
2020-10-25 19:27:56 +0100

    Begun

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod codegen;
+mod parse;
+mod tokenize;
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<_>>();
+
+    if args.len() != 2 {
+        panic!("{}: invalid number of arguments", args[0]);
+    }
+
+    let tokens = tokenize(&args[1]);
+    let node = parse(tokens);
+    codegen(node);
+
+    return 0;
 }
